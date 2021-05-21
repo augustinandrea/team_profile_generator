c790e1d7f2d3d1007ab6465e320f131705bc1cd4
augustinandrea <augustin.andrea@gmail.com>
2021-05-21 12:37:14 -0700

    I added a .then to the add_member function and started working on the html functions.

--- index.js
+++ index.js
@@ -35,24 +35,71 @@ const questions = [
         name: "member_email",
         message: "Input the team member's email address:",
         type: "input"
-    },
-
-    {
-        name: "continue_or_stop",
-        message: "Do you want to make more team members or stop?",
-        type: "list",
-        choices: [
-            "Yes I have another team member to input.",
-            "No, I am finished with inputting team members."
-        ]
     }
+
 ]
 
 // the given functions
 
+// function for adding the different roles and the new sides
+function add_role(name, role, id, email){
+    let role_info = "";
+    if(role === Engineer){
+        role_info = "Github username";
+    }
+    else if(role === Intern ){
+        role_info = "School";
+    }
+    else{
+        role_info = "Office number";
+    }
+
+    inquirer.prompt([
+        {
+            name: "role_info",
+            message: "Choose the team member's role info:",
+            type: "input"
+        },
+
+        {
+            name: "more_members",
+            message: "Do you want to make more team members or stop?",
+            type: "list",
+            choices: [
+                "Yes I have another team member to input.",
+                "No, I am finished with inputting team members."
+            ]
+        }
+    ])
+    .then( function(role_info, more_members){
+        let new_member;
+        if(role === "Engineer"){
+            new_member = new Engineer(name, id, email, role_info);
+        }
+        else if(role === "Intern"){
+            new_member = new Intern(name, id, email, role_info);
+        }
+        else{
+            new_member = new Manager(name, id, email, role_info);
+        }
+
+        employees.push(new_member);
+        add_HTML(new_member)
+        .then(function() {
+            if(more_members === "Yes I have another team member to input."){
+                add_member();
+            }
+            else{
+                final_html();
+            }
+        }
+
+    })
+
+}
+
+
 function add_member(){
     inquirer.prompt(questions)
-    .then(function(name, role, id, email){
-        
-    })
+    .then(results => add_role(results)))
 }
